18ae0dfe10f6fbe72e9e04edd9fbb2159943eae7
milanpaudell <milanpaudell31@gmail.com>
2023-10-12 20:58:32 +0545

    cors

--- app.js
+++ app.js
@@ -1,148 +1,121 @@
-const { connectDatabase } = require('./database/database');
-const Blog = require('./model/blogModel');
-
-
-const express = require('express')
-// const mongoose = require('mongoose')
+const { connectDatabase } = require("./database/database");
+const Blog = require("./model/blogModel");
+const express = require("express")
 const app = express();
+const cors = require("cors")
+
+app.use(cors({
+    origin : "http://localhost:5173",
+
+}))
 
 
-
-// nodeJs lai form bata aako data parse gar vaneko ho 
+// nodejs lai form bata aako data parse gar vaneko ho 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-// Database Connection Function
+// DATABASE CONNECTION FUNCTION
 connectDatabase()
 
-// connectig to database
-// mongoose.connect('mongodb+srv://admin:<password>@cluster0.6g3gudb.mongodb.net/?retryWrites=true&w=majority')
-// .then(() => {
-//     console.log('Database connected')
-// })
 
-
-
-// GET API
-app.get('/',(req,res) => {
-    res.json({
-        status : 200,
-        message : 'Success'
-    })
+// GET API -> /
+app.get("/",(req,res)=>{
+   res.json({
+    message : "I am alive"
+   })
 })
+
 
 // GET API => /blogs (All blogs)
-app.get('/blogs',async (req,res) => {
+app.get("/blogs",async (req,res)=>{
     // fetching/reading all Blogs from Blog model
-    const blogs = await Blog.find()
-    // check if blogs contains data or not
-    if(blogs.length == 0){
-        res.status(404).json({
-            // status : 404,
-            message : 'Empty blogs',
-
-        })
-    }
-    else {
-        res.status(200).json({
-            // status : 200,
-            message : 'Blog fetched successfully',
-            data : blogs
+   const blogs =  await Blog.find()
+   // check if blogs contains data or not
+   if(blogs.length == 0){
+    res.status(404).json({
+        // status : 404,
+        message : "Empty blogs"
+    })
+   }else{
+       res.status(200).json({
+        //    status : 200,
+           message : "Blogs fetched successfully",
+           blogs : blogs
         })
     }
 })
 
+// GET API -> /blogs/:id (single Blog)
+app.get("/blogs/:id",async (req,res)=>{
+   const id = req.params.id
+//    console.log(id)
+//    const {id} = req.params ALTERNATIVE
+//    const blog = await Blog.find({_id :id})
+//    if(blog.length == 0){
+//     res.status(404).json({
+//         message : "No blogs found with that id"
+//     })
+//    }else{
 
-// GET API -> /blogs/:id (single blog)
-app.get('/blogs/:id',async (req,res) => {
-    // console.log(req.params.id)
-
-    const id = req.params.id
-    console.log(id)
-    // const {id} = req.params   //Alternative
-
-
-    // const blog = await Blog.find({_id :id})
-    // if (blog.length == 0) {
-    //     res.status(404).json({
-    //         message : 'Blog not found',
-    //     })
-    // }
-    // else {
-    //     res.status(200).json({
-    //         message : 'Blog fetched successfully',
-    //         data : blog
-    //     })
-    // }
-
-    // Alterntive
-    const blog = await Blog.findById(id) 
+//        res.status(200).json({
+//            messge : "Blog fetched successfully",
+//            data : blog
+//         })
+//     }
+    // ALTERNATIVE 
+    const blog = await Blog.findById(id)
     if(blog){
         res.status(200).json({
-            message : 'Blog fetched successfully',
+            message : "Blog fetched succesfully",
             data : blog
         })
-    }
-    else{
+    }else{
         res.status(404).json({
-            message : 'Blog not found'
+            message : "No blog found"
         })
     }
 
 })
 
 
-// CRETE Blog API
-app.post('/blogs', async(req,res) => {
+// CREATE BLOG API  
+app.post("/blogs", async(req,res)=>{
+   const title = req.body.title;
+   const subTitle = req.body.subTitle
+   const description = req.body.description
+
+//    Alternative (object destructuring)
+//    const {title,subTitle,description} = req.body
+
+    // Insert to database logic goes here 
+   await Blog.create({
+        title : title  ,
+        subTitle : subTitle,
+        description : description
+    })
+    
+
+    res.json({
+        status : 201,
+        message : "Blog created succesfully"
+    })
+    // Alternative 
+    // res.status(200).json({
+    //     message : "Blog created successfully"
+    // })
+})
+
+
+
+// UPDATE BLOG API 
+app.patch("/blogs/:id",async (req,res)=>{
+    const id = req.params.id
     const title = req.body.title
     const subTitle = req.body.subTitle
     const description = req.body.description
     
-    // const {title,subTitle,description} = req.body        // Alternative
-
-    // Insert to database logic goes here
-    await Blog.create({
-        title : title,
-        subTitle : subTitle,
-        description : description
-    })
-
-    // res.json({                                  //Alternative
-    //     status : 201,
-    //     message : 'Blog creates successfully'
-    // })
-
-    res.status(200).json({               
-        message : 'Blog creates successfully'
-    })
-})
-
-
-// UPDATE Blog API
-app.patch('/blogs/:id',async (req,res) => {
-    const id = req.params.id
-    const title = req.body.title
-    const subTitle = req.body.subTitle
-    const description = req.body.description
-
-    // const title = req.body.title
-    // const foundBlogWithTitle = await Blog.find({
-    //     title : title
-    // })
-    // console.log(foundBlogWithTitle)
-    // foundBlogWithTitle[0].description = description
-    // foundBlogWithTitle[0].subTitle = subTitle
-    // await foundBlogWithTitle.save()
-    // const {title,subTitle,description} = req.body      // Alternative
-
-    // Check if blog already exists or not
-    // const isBlogFound = await Blog.find({
-    //     id :id
-    // })
-    // if(isBlogFound.length == 0)
-    // res.json({
-    //     message : 'No blogs found'
-    // })
+    // const {title,subTitle,description} = req.body  ALTERNATIVE
+    
 
     await Blog.findByIdAndUpdate(id,{
         title : title,
@@ -151,22 +124,26 @@ app.patch('/blogs/:id',async (req,res) => {
     })
 
     res.status(200).json({
-        message : 'Blog updated successfully'
+        message : "Blog updated Succesfully"
     })
+
 })
 
-// DELETE API
-app.delete('/blogs/:id', async (req, res) =>{
+// DELETE API 
+app.delete("/blogs/:id",async (req,res)=>{
     const id = req.params.id
-    // const {id} = req.params
+    // const {id} = req.params 
 
     await Blog.findByIdAndDelete(id)
 
     res.status(200).json({
-        message : 'Blog deleted successfully'
+        message : "Blog Deleted Successfully"
     })
 })
 
-app.listen(2000,() => {
-    console.log('NodeJS listening on port 2000')
+
+
+
+app.listen(2000,()=>{
+    console.log("Nodejs has started at port 2000")
 })
